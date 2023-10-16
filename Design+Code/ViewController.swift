7256f033818b0f6610d85869c92e61b2da48bc7f
Pavel Markitantov <markitantow@gmail.com>
2023-10-16 19:48:24 +0200

    feat: added a folder that makes it easier to create new Views

--- Design+Code/ViewController.swift
+++ Design+Code/ViewController.swift
@@ -8,12 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var blurView: UIVisualEffectView!
+    @IBOutlet var cardView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    
     }
-
-
 }
-
